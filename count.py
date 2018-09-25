503c0d17651bf7123321b2ebe63770d4d3d448c9
Sponja- <tomas.spognardi@gmail.com>
2018-09-25 20:17:32 -0300

    Fixed count.py

--- count.py
+++ count.py
@@ -3,26 +3,27 @@ from image_utils import *
 from segmentation import *
 from keras.models import load_model
 
-def center_of_segment(segments, value):
-	mask = mask_from_segments(segments, value)
-	positions = mask.nonzero()
-	length = len(positions[0])
-	sum_x, sum_y = sum(positions[0]), sum(positions[1])
-	return (sum_x / length, sum_y / length)
-
 if __name__ == "__main__":
 
 	image = load("input.jpg")
 	segments = segment(image)
-	padded_segments = np.array(images_from_selection(image, segments, range(segments.max() + 1)))
+
+	padded_segments = []
+	segment_val = []
+	for i in range(segments.max() + 1):
+		img = padded_image(image, segments, i)
+		if img is not None:
+			padded_segments.append(img)
+			segment_val.append(i)
+	padded_segments = np.array(padded_segments)
 
 	model = load_model("model")
 	predictions = model.predict(padded_segments)
 
 	count = 0
-	for i, pred in enumerate(predictions):
+	for i, pred in zip(segment_val, predictions):
 		if pred[0] > pred[1]:
-			image[center_of_segment(segments, i)] = [255, 0, 0]
+			image[segments == i] = [255, 0, 0]
 			count += 1
 
 	print(f"Found {count} cars")
